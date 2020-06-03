32140843a1f02ddc7886229368dfa417c86de367
amr-Alkhayat <amralkhayat@gmail.com>
2020-06-03 23:36:35 +0200

    Initial Commit

---
+++ FaceDetection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FaceDetection
+//
+//  Created by amrpc on 6/3/20.
+//  Copyright © 2020 amrpc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
